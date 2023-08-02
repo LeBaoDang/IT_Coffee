c15db69cfbfeb7b38690b9e5e72f608a6d6e76e3
Cosker Dang (BEDL1) <111851933+CoskerDangBEDL1@users.noreply.github.com>
2023-08-02 08:11:25 +0700

    update_package

--- test/com/itcoffee/ui/DangNhapDiaLogIT.java
+++ test/com/itcoffee/ui/DangNhapDiaLogIT.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
  */
-package Com.itcoffee.ui;
+package com.itcoffee.ui;
 
+import com.itcoffee.ui.DangNhapDiaLog;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
